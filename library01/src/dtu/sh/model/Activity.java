80c50bcaee02071db3b20972e52dd0d0c2b5fc02
Mikkel Muller-Hansen <mikmhansen@yahoo.dk>
2018-04-09 16:49:23 +0200

    Added Project steps and added constructors to the nessercery class files

--- library01/src/dtu/sh/model/Activity.java
+++ library01/src/dtu/sh/model/Activity.java
@@ -1,5 +1,22 @@
 package dtu.sh.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public abstract class Activity {
 
+	private String description;
+	private Date startDate;
+	private Date endDate;
+	
+	public List<TimeRegistration> timeRegistrations;
+	
+	public Activity(String description, Date start, Date end) {
+		this.description = description;
+		this.startDate = start;
+		this.endDate = end;
+		
+		timeRegistrations = new ArrayList<TimeRegistration>();
+	}
 }
--- library01/src/dtu/sh/model/ProjectActivity.java
+++ library01/src/dtu/sh/model/ProjectActivity.java
@@ -1,6 +1,16 @@
 package dtu.sh.model;
 
+import java.util.Date;
+
 public class ProjectActivity extends Activity {
+
+	private int priority;
+	
+	public ProjectActivity(String description, Date start, Date end, int priority) {
+		super(description, start, end);
+
+		this.priority = priority;
+	}
 
 	
 }
