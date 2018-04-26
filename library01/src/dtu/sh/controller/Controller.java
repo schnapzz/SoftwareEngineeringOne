66f8072a3ed487e23f95bb6613780ef96a87a8e0
Helena Schiøtz <s174279@student.dtu.dk>
2018-04-26 12:57:44 +0200

    afdf

--- library01/src/dtu/sh/controller/Controller.java
+++ library01/src/dtu/sh/controller/Controller.java
@@ -4,16 +4,24 @@ import javax.swing.JFrame;
 
 import dtu.sh.main.LoggedIn;
 import dtu.sh.main.Main;
+import dtu.sh.model.SH;
 
 public class Controller {
 	private JFrame frame;
 	private LoggedIn loginWindow;
 	private Main mainWindow;
+	private SH sh;
 	
 	public Controller() {
 		this.loginWindow = new LoggedIn();
 		this.mainWindow = new Main();
 	}
+	
+//	public Controller(SH sh) {
+//		this.loginWindow = new LoggedIn();
+//		this.mainWindow = new Main();
+//		this.sh = sh;
+//	}
 	
 //	public void login(){
 //		frame.dispose();
@@ -26,6 +34,7 @@ public class Controller {
 		frame.dispose();
 		LoggedIn log = new LoggedIn();
 		log.setVisible(true);
+		//sh.logInEmployee(username);
 	}
 
 	
