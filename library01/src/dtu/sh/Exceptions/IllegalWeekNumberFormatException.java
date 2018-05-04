0d3a3670e6ef5f5d1fc10c61971b6831c6eea85b
Mikkel Muller-Hansen <mikmhansen@yahoo.dk>
2018-05-04 23:06:06 +0200

    Added missing exception file

---
+++ library01/src/dtu/sh/Exceptions/IllegalWeekNumberFormatException.java
@@ -0,0 +1,15 @@
+package dtu.sh.Exceptions;
+
+// Mikkel
+public class IllegalWeekNumberFormatException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public IllegalWeekNumberFormatException() {
+	    super("Illegal week number format");
+	}
+
+}
