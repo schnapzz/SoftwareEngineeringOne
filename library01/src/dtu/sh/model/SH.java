820ec8b43121cad59ac1fa7f5f310b10f9899fdd
Helena Schiøtz <s174279@student.dtu.dk>
2018-05-05 23:03:37 +0200

    report can now be shown

--- library01/src/dtu/sh/model/SH.java
+++ library01/src/dtu/sh/model/SH.java
@@ -155,8 +155,8 @@ public class SH {
 	public Report requestReport(Project project, String id) {
 		for (Project p: projects) {
 			if (p.getTitle().equals(project.getTitle()) && p.getProjectLeader().equals(id)) {
-				Report report = p.createReport();
-				return report;
+				Report r = p.createReport();
+				return r;
 			}
 		}
 		return null;
